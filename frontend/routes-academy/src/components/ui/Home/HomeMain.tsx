3cf60b712775dc44a465c58af0f6d77e65dbdb40
Harii2005 <harikrishnankrkv@gmail.com>
2025-10-15 19:10:45 +0530

    companies , Metrices , Faq

--- frontend/routes-academy/src/components/ui/Home/HomeMain.tsx
+++ frontend/routes-academy/src/components/ui/Home/HomeMain.tsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import HeroHome from './HeroHome'
-import TestimonialVideo from './TestimonialVideo'
-import Courses from './TopCourses'
-import Faq from './Faq'
-import Alumni from './Alumini'
-import Metrices from "./Metrics"
-import Companies from './Companies'
+import React from "react";
+import HeroHome from "./HeroHome";
+import TestimonialVideo from "./TestimonialVideo";
+import Courses from "./TopCourses";
+import Faq from "./Faq";
+import Alumni from "./Alumini";
+import Metrices from "./Metrics";
+import Companies from "./Companies";
 
 const HomeMain = () => {
   return (
     <div>
-        <HeroHome/>
-        <TestimonialVideo />
-        <Alumni />
-        <Metrices />
-        <Companies />
-        <Courses />
-        <Faq />
+      <HeroHome />
+      <TestimonialVideo />
+      <Alumni />
+      <Metrices />
+      <Companies />
+      <Courses />
+      <Faq />
     </div>
-  )
-}
+  );
+};
 
-export default HomeMain
+export default HomeMain;
