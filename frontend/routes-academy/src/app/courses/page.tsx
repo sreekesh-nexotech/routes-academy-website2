84859fb550a97f0cd97096c895c183535172c543
sarath paul <sarathpaul23@gmail.com>
2025-10-16 11:41:27 +0530

    Add placements course contact section

--- frontend/routes-academy/src/app/courses/page.tsx
+++ frontend/routes-academy/src/app/courses/page.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import CourseHero from "@/components/ui/Courses/CourseHero";
+import CourseContact from "@/components/ui/Courses/CourseContact";
 
 const page = () => {
   return (
     <div>
       <CourseHero />
+      <div className="px-[1rem] py-[2rem] sm:p-[5rem] bg-[#104EFF]">
+        <h2 className="text-center text-[2.25rem] text-[#FFFFFF] font-bold leading-[2.875rem]">
+          A 90%+ placement rate within six months
+        </h2>
+      </div>
+      <CourseContact />
     </div>
   );
 };
