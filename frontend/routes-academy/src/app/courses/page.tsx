3dc180f1a14e148321f4bdd2972da0c5475602c4
sarath paul <sarathpaul23@gmail.com>
2025-10-16 23:29:06 +0530

    Move components from courses page to CourseMain and render CourseMain in courses page(only refactoring)

--- frontend/routes-academy/src/app/courses/page.tsx
+++ frontend/routes-academy/src/app/courses/page.tsx
@@ -1,22 +1,11 @@
 import React from "react";
-import CourseHero from "@/components/ui/Courses/CourseHero";
-import CourseContact from "@/components/ui/Courses/CourseContact";
-import Courses from "@/components/ui/Courses/Courses";
-import CourseHelps from "@/components/ui/Courses/CourseHelps";
+import CourseMain from "@/components/ui/Courses/CourseMain";
 
 const page = () => {
   return (
-    <div>
-      <CourseHero />
-      <div className="px-[1rem] py-[2rem] sm:p-[5rem] bg-[#104EFF]">
-        <h2 className="text-center text-[2.25rem] text-[#FFFFFF] font-bold leading-[2.875rem]">
-          A 90%+ placement rate within six months
-        </h2>
-      </div>
-      <Courses />
-      <CourseHelps />
-      <CourseContact />
-    </div>
+    <>
+      <CourseMain />
+    </>
   );
 };
 
