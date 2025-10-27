1f8f4c684ab89f8e34ac27dd37e152b4ebbaeaa4
Harii2005 <harikrishnankrkv@gmail.com>
2025-10-27 18:29:26 +0530

    data added

--- frontend/routes-academy/src/components/ui/Courses/Courses.tsx
+++ frontend/routes-academy/src/components/ui/Courses/Courses.tsx
@@ -27,24 +27,9 @@ const Courses = () => {
               <GuideMe />
             </div>
           </div>
-          <div className="lg:col-span-3 lg:h-screen lg:overflow-y-auto scrollbar-1">
+          <div className="lg:col-span-3">
             <CourseList />
           </div>
-        </div>
-        <div className="flex gap-[1rem] md:gap-[3.0625rem]">
-          <hr className="w-full h-[1px] my-8 bg-[#104EFF] border-0 rounded-sm dark:bg-gray-700"></hr>
-          <button className="cursor-pointer bg-[#104EFF] min-w-[12.625rem] h-[3.5625rem] rounded-[.25rem] flex gap-[.625rem] justify-center items-center">
-            <h6 className="font-semibold text-[#FFFFFF] text-[1rem] leading-[1.375rem]">
-              See All Courses
-            </h6>
-            <Image
-              src="/mingcute_down-line.svg"
-              alt=""
-              width={33}
-              height={33}
-            />
-          </button>
-          <hr className="w-full h-[1px] my-8 bg-[#104EFF] border-0 rounded-sm dark:bg-gray-700"></hr>
         </div>
       </div>
     </div>
@@ -99,7 +84,7 @@ function CourseList() {
 function CourseCard({ course }: { course: Course }) {
   return (
     <Link href={`/courses/${course.id}`}>
-      <div className="max-w-[26.65625rem] h-full rounded-[1rem] bg-[#FFFFFF] shadow-md flex flex-col">
+      <div className="max-w-[28rem] h-full rounded-[1rem] bg-[#FFFFFF] shadow-md flex flex-col">
         <Image
           src={course.image_url}
           alt={`coursecard-${course.id} image`}
@@ -110,7 +95,7 @@ function CourseCard({ course }: { course: Course }) {
           }`}
         />
         <div className="p-[1rem] flex flex-col flex-grow">
-          <h5 className="font-bold text-[#172554] text-[1.125rem] md:text-[1.3125rem] leading-[1.5rem] md:leading-[1.875rem] mb-[1rem] min-h-[3rem]">
+          <h5 className="font-bold text-[#172554] text-[1.125rem] md:text-[1.3125rem] leading-[1.5rem] md:leading-[1.875rem] mb-[0.1rem] min-h-[3rem]">
             {course.title}
           </h5>
           <p className="font-(family-name:--font-onest) font-normal text-[#404040] text-[.75rem] md:text-[.875rem] leading-[1.125rem] mb-[1rem] flex-grow">
