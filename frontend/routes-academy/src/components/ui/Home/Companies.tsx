92a283f52a58a2a676b38b45143f49fd2afb5250
Harii2005 <harikrishnankrkv@gmail.com>
2025-11-04 14:30:54 +0530

    added new certificate logo

--- frontend/routes-academy/src/components/ui/Home/Companies.tsx
+++ frontend/routes-academy/src/components/ui/Home/Companies.tsx
@@ -5,10 +5,26 @@ import Image from "next/image";
 
 const companiesData = [
   { src: "https://routesacademy.b-cdn.net/icons/ibm.png", alt: "IBM" },
-  { src: "https://routesacademy.b-cdn.net/icons/Microsoft_logo_(2012).png", alt: "Microsoft" },
-  { src: "https://routesacademy.b-cdn.net/icons/idnIFWhoAM_logos.png", alt: "BFSI" },
-  { src: "https://routesacademy.b-cdn.net/icons/STED.a89f7934.png", alt: "STED" },
-  { src: "https://routesacademy.b-cdn.net/icons/SAP_2011_logo.png", alt: "SAP" },
+  {
+    src: "https://routesacademy.b-cdn.net/icons/Microsoft_logo_(2012).png",
+    alt: "Microsoft",
+  },
+  {
+    src: "https://routesacademy.b-cdn.net/icons/idnIFWhoAM_logos.png",
+    alt: "BFSI",
+  },
+  {
+    src: "https://routesacademy.b-cdn.net/icons/732d296dbd6c95ab945e2be9ce1e0d16.png",
+    alt: "Certification",
+  },
+  {
+    src: "https://routesacademy.b-cdn.net/icons/STED.a89f7934.png",
+    alt: "STED",
+  },
+  {
+    src: "https://routesacademy.b-cdn.net/icons/SAP_2011_logo.png",
+    alt: "SAP",
+  },
 ];
 
 const Companies: React.FC = () => {
@@ -20,18 +36,18 @@ const Companies: React.FC = () => {
       </h2>
 
       {/* Logos Row */}
-      <div className="flex flex-wrap justify-center items-center gap-[4vw] w-full max-w-[90vw]">
+      <div className="flex flex-wrap justify-center items-center gap-[2vw] md:gap-[3vw] w-full max-w-[95vw]">
         {companiesData.map((cert, index) => (
           <div
             key={index}
-            className="flex items-center justify-center h-[10vh] w-[20vw] min-w-[120px] max-w-[200px]"
+            className="flex items-center justify-center h-[9vh] w-[18vw] min-w-[90px] max-w-[140px]"
           >
             <Image
               src={cert.src}
               alt={cert.alt}
-              width={200}
+              width={180}
               height={100}
-              className="object-contain w-[80%] h-[80%]"
+              className="object-contain w-[75%] h-[75%]"
               unoptimized
             />
           </div>
