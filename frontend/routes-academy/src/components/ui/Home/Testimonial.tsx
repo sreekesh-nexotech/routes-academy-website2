bf2be14c7b1c75ba27fe9c67d94e43bd39cacd4c
Sreyes V <sreyesv24@gmail.com>
2025-11-21 21:12:05 +0530

    updated alignment issue in mobile

--- frontend/routes-academy/src/components/ui/Home/Testimonial.tsx
+++ frontend/routes-academy/src/components/ui/Home/Testimonial.tsx
@@ -43,12 +43,16 @@ const testimonials = [
   {
     image: 'https://routesacademy.b-cdn.net/images/testimonial%20(4).png',
     name: 'Sreedhul K',
-    role: '',
+    role: 'intern at an MNC',
     courses: [
       
       { 
         icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z"></path></svg>,
         title: 'SAP Material Management' 
+      },
+      { 
+        icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z"></path></svg>,
+        title: 'SAP' 
       },
     ],
     stats: [
@@ -60,12 +64,16 @@ const testimonials = [
   {
     image: 'https://routesacademy.b-cdn.net/images/testimonial%20(1).png',
     name: 'Karthikeyan T P',
-    role: '',
+    role: 'intern at an MNC',
     courses: [
       
       { 
         icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z"></path></svg>,
         title: 'SAP Material Management' 
+      },
+      { 
+        icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z"></path></svg>,
+        title: 'SAP' 
       },
     ],
     stats: [
